ec9225838015779e4a63b8929f84bc1459940050
Telokis <telokis0@gmail.com>
2022-10-16 19:24:19 +0200

    Made JS example work.

--- examples/javascript/src/default_code.js
+++ examples/javascript/src/default_code.js
@@ -3,42 +3,32 @@
 // If you don't know how to code, don't worry, It's easy.
 // Just set attack_mode to true and ENGAGE!
 
-var attack_mode=false
+var attack_mode = false;
 
-setInterval(function(){
+setInterval(function () {
+  use_hp_or_mp();
+  loot();
 
-	use_hp_or_mp();
-	loot();
+  if (!attack_mode || character.rip || is_moving(character)) return;
 
-	if(!attack_mode || character.rip || is_moving(character)) return;
+  var target = get_targeted_monster();
+  if (!target) {
+    target = get_nearest_monster({ min_xp: 100, max_att: 120 });
+    if (target) change_target(target);
+    else {
+      set_message("No Monsters");
+      return;
+    }
+  }
 
-	var target=get_targeted_monster();
-	if(!target)
-	{
-		target=get_nearest_monster({min_xp:100,max_att:120});
-		if(target) change_target(target);
-		else
-		{
-			set_message("No Monsters");
-			return;
-		}
-	}
-	
-	if(!is_in_range(target))
-	{
-		move(
-			character.x+(target.x-character.x)/2,
-			character.y+(target.y-character.y)/2
-			);
-		// Walk half the distance
-	}
-	else if(can_attack(target))
-	{
-		set_message("Attacking");
-		attack(target);
-	}
-
-},1000/4); // Loops every 1/4 seconds.
+  if (!is_in_range(target)) {
+    move(character.x + (target.x - character.x) / 2, character.y + (target.y - character.y) / 2);
+    // Walk half the distance
+  } else if (can_attack(target)) {
+    set_message("Attacking");
+    attack(target);
+  }
+}, 1000 / 4); // Loops every 1/4 seconds.
 
 // Learn Javascript: https://www.codecademy.com/learn/introduction-to-javascript
 // Write your own CODE: https://github.com/kaansoral/adventureland
