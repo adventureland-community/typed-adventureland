26ec27019d82b3d35283fb869a73ffcb0bd4a1cf
thmsn <9084377+thmsndk@users.noreply.github.com>
2022-10-23 20:58:16 +0200

    Utilize BetterUXWrapper to give a better feeling when using the generalized GItem for example

---
+++ src/types/GTypes/utils.ts
@@ -0,0 +1,13 @@
+// From https://www.npmjs.com/package/@telokys/ts-meta-types#debugterface
+export type Debug<Terface> = Terface extends infer O ? { [K in keyof O]: O[K] } : never;
+
+// Creates a union of all keys of all objects in the Terface union
+export type AllKeys<Terface> = Terface extends any ? (keyof Terface & (string | number | symbol)) : never
+
+// Creates a new interface adding the missing keys to Terface
+export type Wrap<Terface, Keys extends string | number | symbol> = (Terface & {
+    [K in Exclude<Keys, keyof Terface>]?: undefined;
+})
+
+// Distributes the union and automatically add the missing keys
+export type BetterUXWrapper<Terface, Keys extends AllKeys<Terface> = AllKeys<Terface>> = Terface extends any ? Wrap<Terface, Keys> : never;
