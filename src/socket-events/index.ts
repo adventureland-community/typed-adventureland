a36703ada8f820526481626b4b95c35b730854a7
Telokis <telokis0@gmail.com>
2022-12-11 19:13:42 +0100

    Add missing socket events exports

--- src/socket-events/index.ts
+++ src/socket-events/index.ts
@@ -2,6 +2,9 @@ import { TypedEventEmitter } from "../TypedEventEmitter";
 import { ClientToServerEvents } from "./client-to-server";
 import { ServerToClientEvents } from "./server-to-client";
 
+export * from "./client-to-server";
+export * from "./server-to-client";
+
 export type SocketWithEventsFunctions = Pick<
   TypedEventEmitter<ServerToClientEvents>,
   "on" | "once" | "off"
