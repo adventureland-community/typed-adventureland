3c522751984a4063487f4dfbb8243bce26daaee9
Kent Rasmussen <hyprkookeez@gmail.com>
2024-09-07 15:58:34 +0900

    Add S to welcome socket

--- src/socket-events/server-to-client/events/ServerToClient_welcome.ts
+++ src/socket-events/server-to-client/events/ServerToClient_welcome.ts
@@ -1,10 +1,12 @@
 import { CharacterEntity } from "../../../entity";
 import { MapKey } from "../../../G";
 import { ServerIdentifier, ServerRegion } from "../../../server";
+import { ServerToClient_server_info } from "./ServerToClient_server_info";
 
 export type ServerToClient_welcome = {
   /** The character gets returned if you open a socket with a secret (i.e. /comm and click on one of your characters) */
   character?: CharacterEntity;
+  S: ServerToClient_server_info;
   region: ServerRegion;
   in: string;
   map: MapKey;
